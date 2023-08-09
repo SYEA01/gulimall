867b2bf8afb0c26ea489ad3eecfb02347684ad77
taoao <taoao0101@163.com>
2023-08-10 01:02:13 +0800

    使用Nacos作为注册中心

--- gulimall-coupon/src/main/java/com/example/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/example/gulimall/coupon/GulimallCouponApplication.java
@@ -2,12 +2,12 @@ package com.example.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // Nacos注册中心 开启服务注册与发现 功能
 public class GulimallCouponApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GulimallCouponApplication.class, args);
     }
-
 }
