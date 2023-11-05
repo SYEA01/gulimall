9ecbfeef10b8b9717716e0e6ea5577bbfc713f1b
taoao <taoao0101@163.com>
2023-11-05 14:08:52 +0800

    gateway 网关基本配置

---
+++ gulimall-gateway/src/main/java/com/example/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.example.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})   // 排除跟数据库有关的配置
+@EnableDiscoveryClient  // 开启服务的注册发现
+public class GulimallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+}
