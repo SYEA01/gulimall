fb43fd05dc6c421d73226f50ead2dc180e4d1a0b
taoao <taoao0101@163.com>
2023-11-25 20:08:20 +0800

    P46-商品服务-三级分类-配置网关路由与路径重写

--- renren-fast/src/main/java/io/renren/RenrenApplication.java
+++ renren-fast/src/main/java/io/renren/RenrenApplication.java
@@ -10,9 +10,11 @@ package io.renren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient  // 将renren-fast注册到注册中心中
 public class RenrenApplication {
 
 	public static void main(String[] args) {
