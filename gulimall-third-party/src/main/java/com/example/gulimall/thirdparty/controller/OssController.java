a6deff2fe520e86916bf6ca4307b025ca549f709
taoao <taoao0101@163.com>
2023-12-02 19:35:57 +0800

    P63-商品服务-API-品牌管理- OSS前后端联调测试上传

--- gulimall-third-party/src/main/java/com/example/gulimall/thirdparty/controller/OssController.java
+++ gulimall-third-party/src/main/java/com/example/gulimall/thirdparty/controller/OssController.java
@@ -54,7 +54,7 @@ public class OssController {
             String postSignature = ossClient.calculatePostSignature(postPolicy);
 
             respMap = new LinkedHashMap<String, String>();
-            respMap.put("accessid", accessId);
+            respMap.put("accessId", accessId);
             respMap.put("policy", encodedPolicy);
             respMap.put("signature", postSignature);
             respMap.put("dir", dir);
