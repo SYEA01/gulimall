9704e3cecd1f7cf3a418ec094782c764d7501734
taoao <taoao0101@163.com>
2023-12-02 15:04:15 +0800

    P62-商品服务-品牌管理- 原生OSS整合测试

--- gulimall-product/src/test/java/com/example/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/example/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.gulimall.product.entity.BrandEntity;
 import com.example.gulimall.product.service.BrandService;
@@ -9,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -16,6 +21,23 @@ import java.util.List;
 public class GulimallProductApplicationTests {
     @Autowired
     private BrandService brandService;
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // Endpoint
+        String endpoint = "oss-cn-beijing.aliyuncs.com";
+        String accessKeyId = "LTAI5tEBjMBdeVSmkzpesNJh";
+        String accessKeySecret = "a0Rx3CNeogfdLh7DxW3vzGfIkJjgDU";
+        // 创建OSSClient实例
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        InputStream inputStream = new FileInputStream("D:\\百度网盘下载路径\\docs\\pics\\1f15cdbcf9e1273c.jpg");
+        ossClient.putObject("gulimall-taoao", "AA.jpg", inputStream);
+
+        ossClient.shutdown();
+
+        System.out.println("上传完成");
+    }
 
     @Test
     public void contextLoads() {
