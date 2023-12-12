e5476b8e50a2122af0342bdb2042ff59a00fd572
taoao <taoao0101@163.com>
2023-12-12 22:39:00 +0800

    P88-商品服务-API-新增商品-保存spu基本信息

--- gulimall-product/src/main/java/com/example/gulimall/product/service/SpuInfoService.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/service/SpuInfoService.java
@@ -2,6 +2,7 @@ package com.example.gulimall.product.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.common.utils.PageUtils;
+import com.example.gulimall.product.entity.SpuInfoDescEntity;
 import com.example.gulimall.product.entity.SpuInfoEntity;
 import com.example.gulimall.product.vo.SpuSaveVo;
 
@@ -19,5 +20,12 @@ public interface SpuInfoService extends IService<SpuInfoEntity> {
     PageUtils queryPage(Map<String, Object> params);
 
     void saveSpuInfo(SpuSaveVo vo);
+
+    /**
+     * 保存Spu基本信息
+     * @param spuInfoEntity
+     */
+    void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity);
+
 }
 
--- gulimall-product/src/main/java/com/example/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,8 +1,20 @@
 package com.example.gulimall.product.service.impl;
 
+import com.example.gulimall.product.entity.AttrEntity;
+import com.example.gulimall.product.entity.ProductAttrValueEntity;
+import com.example.gulimall.product.entity.SpuInfoDescEntity;
+import com.example.gulimall.product.service.*;
+import com.example.gulimall.product.vo.BaseAttrs;
 import com.example.gulimall.product.vo.SpuSaveVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,12 +23,23 @@ import com.example.common.utils.Query;
 
 import com.example.gulimall.product.dao.SpuInfoDao;
 import com.example.gulimall.product.entity.SpuInfoEntity;
-import com.example.gulimall.product.service.SpuInfoService;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("spuInfoService")
 public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> implements SpuInfoService {
+
+    @Autowired
+    SpuInfoDescService spuInfoDescService;
+
+    @Autowired
+    SpuImagesService spuImagesService;
+
+    @Autowired
+    AttrService attrService;
+
+    @Autowired
+    ProductAttrValueService productAttrValueService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -32,12 +55,38 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
     @Override
     public void saveSpuInfo(SpuSaveVo vo) {
         // 1、保存spu基本信息 pms_spu_info
+        SpuInfoEntity spuInfoEntity = new SpuInfoEntity();
+        BeanUtils.copyProperties(vo, spuInfoEntity);
+        spuInfoEntity.setCreateTime(new Date());
+        spuInfoEntity.setUpdateTime(new Date());
+        this.saveBaseSpuInfo(spuInfoEntity);
 
         // 2、保存spu的描述图片 pms_spu_info_desc
+        List<String> decript = vo.getDecript();
+        SpuInfoDescEntity spuInfoDescEntity = new SpuInfoDescEntity();
+        Long spuId = spuInfoEntity.getId();
+        spuInfoDescEntity.setSpuId(spuId);
+        spuInfoDescEntity.setDecript(String.join(",", decript));
+        spuInfoDescService.saveSpuInfoDesc(spuInfoDescEntity);
 
         // 3、保存spu的图片集 pms_spu_images
+        List<String> images = vo.getImages();
+        spuImagesService.saveImages(spuId, images);
 
         // 4、保存spu的规格参数  pms_product_attr_value
+        List<BaseAttrs> baseAttrs = vo.getBaseAttrs();
+        List<ProductAttrValueEntity> collect = baseAttrs.stream().map(attr -> {
+            ProductAttrValueEntity productAttrValueEntity = new ProductAttrValueEntity();
+            Long attrId = attr.getAttrId();
+            productAttrValueEntity.setAttrId(attrId);
+            AttrEntity attrEntity = attrService.getById(attrId);
+            productAttrValueEntity.setAttrName(attrEntity.getAttrName());
+            productAttrValueEntity.setAttrValue(attr.getAttrValues());
+            productAttrValueEntity.setQuickShow(attr.getShowDesc());
+            productAttrValueEntity.setSpuId(spuId);
+            return productAttrValueEntity;
+        }).collect(Collectors.toList());
+        productAttrValueService.saveProductAttr(collect);
 
         // 5、保存spu的积分信息  sms_spu_bounds
 
@@ -51,5 +100,11 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         // 6.4、保存sku的优惠满减等信息  sms_sku_ladder  \  sms_sku_full_reduction  \  sms_member_price  \
 
     }
+
+    @Override
+    public void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity) {
+        this.baseMapper.insert(spuInfoEntity);
+    }
+
 
 }
