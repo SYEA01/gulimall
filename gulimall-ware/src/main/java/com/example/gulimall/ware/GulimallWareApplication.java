abf2b917725d4a9517969a0d562a940035e1fbda
taoao <taoao0101@163.com>
2023-12-14 23:12:10 +0800

    P95-仓储服务-API-仓库管理-将ware服务注册到注册中心中

--- gulimall-ware/src/main/java/com/example/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/example/gulimall/ware/GulimallWareApplication.java
@@ -1,9 +1,15 @@
 package com.example.gulimall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableDiscoveryClient  // 开启服务的注册发现功能
+@MapperScan("com.example.gulimall.ware.dao")  // 开启MyBatis注解扫描
+@EnableTransactionManagement  // 开启事务功能
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
