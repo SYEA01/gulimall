150e55b8e011fd56c5c36c03561dc49522907671
taoao <taoao0101@163.com>
2023-12-19 00:50:26 +0800

    P126-全文检索-ElasticSearch-整合-SpringBoot整合elasticsearch-rest-high-level-client

---
+++ gulimall-search/src/main/java/com/example/gulimall/search/GulimallSearchApplication.java
@@ -0,0 +1,18 @@
+package com.example.gulimall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author taoao
+ */
+@EnableDiscoveryClient  // 启用服务的注册发现
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)  // 如果没有配置数据源相关的配置，但是导入了MyBatis之类的依赖，就需要在主方法这里排除掉
+public class GulimallSearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallSearchApplication.class, args);
+    }
+
+}
---
+++ gulimall-search/src/main/java/com/example/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -0,0 +1,27 @@
+package com.example.gulimall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author taoao
+ * 整合 elasticsearch-rest-high-level-client 步骤
+ * 1、导入依赖
+ * 2、编写配置：给容器中注入一个RestHighLevelClient
+ */
+@Configuration
+public class GulimallElasticSearchConfig {
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(  // 它里面可以指定多个HttpHost
+                        new HttpHost("192.168.100.135", 9200, "http")
+                )
+        );
+        return client;
+    }
+}
