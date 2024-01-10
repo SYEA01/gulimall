c4899bbfd895dc03c65cfb1074f168cb751c02b6
taoao <taoao0101@163.com>
2024-01-10 23:15:03 +0800

    P193-商城业务-异步-异步（第二种初始化多线程：实现Runnable接口）

--- gulimall-search/src/main/java/com/example/gulimall/search/thread/ThreadTest.java
+++ gulimall-search/src/main/java/com/example/gulimall/search/thread/ThreadTest.java
@@ -8,12 +8,15 @@ public class ThreadTest {
         System.out.println("main...start...");
         /**
          * - 1）、继承Thread
+         *         Thread01 thread = new Thread01();
+         *         thread.start();  // 启动线程
          * - 2）、实现Runnable接口
          * - 3）、实现Callable 接口 + FutureTask（可以拿到返回结果，可以处理异常）
          * - 4）、线程池
          */
-        Thread01 thread = new Thread01();
-        thread.start();  // 启动线程
+
+        Runnable01 runnable01 = new Runnable01();
+        new Thread(runnable01).start();
 
         System.out.println("main...end...");
 
@@ -27,7 +30,19 @@ public class ThreadTest {
         public void run() {
             System.out.println("当前线程： " + Thread.currentThread().getId());
             int i = 10 / 2;
-            System.out.println("运行结果：" + i);
+            System.out.println("Thread01运行结果：" + i);
+        }
+    }
+
+    /**
+     * 2）、实现Runnable接口
+     */
+    public static class Runnable01 implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("当前线程： " + Thread.currentThread().getId());
+            int i = 10 / 2;
+            System.out.println("Runnable01运行结果：" + i);
         }
     }
 
