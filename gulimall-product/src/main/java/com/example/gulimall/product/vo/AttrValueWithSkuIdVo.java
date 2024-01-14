c798e55faeb22a2f764c661ce45ad35a64e94eb2
taoao <taoao0101@163.com>
2024-01-14 20:57:38 +0800

    P208-商城业务-商品详情-销售属性渲染

---
+++ gulimall-product/src/main/java/com/example/gulimall/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,12 @@
+package com.example.gulimall.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author taoao
+ */
+@Data
+public class AttrValueWithSkuIdVo {
+    private String attrValue;
+    private String skuIds;
+}
