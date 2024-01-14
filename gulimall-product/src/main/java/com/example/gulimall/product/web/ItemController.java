3c837f42a632ec0161cedbaaeaad3b3bc2befefa
taoao <taoao0101@163.com>
2024-01-14 14:25:09 +0800

    P203-商城业务-商品详情-环境搭建

---
+++ gulimall-product/src/main/java/com/example/gulimall/product/web/ItemController.java
@@ -0,0 +1,26 @@
+package com.example.gulimall.product.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 商品详情信息
+ *
+ * @author taoao
+ */
+@Controller
+public class ItemController {
+
+    /**
+     * 展示当前sku的详情
+     *
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/{skuId}.html")
+    public String skuItem(@PathVariable("skuId") Long skuId) {
+        System.out.println("准备查询：" + skuId + " 的详情");
+        return "item";
+    }
+}
