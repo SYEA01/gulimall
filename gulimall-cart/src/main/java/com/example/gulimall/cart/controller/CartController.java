3764fd41b7523d4db85a4d851f282c3fa65af8fa
taoao <taoao0101@163.com>
2024-02-02 00:53:42 +0800

    P240-商城业务-购物车-页面环境搭建

--- gulimall-cart/src/main/java/com/example/gulimall/cart/controller/CartController.java
+++ gulimall-cart/src/main/java/com/example/gulimall/cart/controller/CartController.java
@@ -19,20 +19,31 @@ public class CartController {
      * 浏览器有一个cookie：user-key：标识用户身份，一个月后过期
      * 如果第一次使用京东的购物车功能，都会给一个临时的user-key
      * 浏览器以后保存，每次访问都会带上这个cookie；
-     *
+     * <p>
      * 如果登录了，session中有
      * 如果没登录，就按照cookie里面带的user-key来做
      * 第一次；如果没有临时用户，帮忙创建一个临时用户
+     *
      * @return
      */
     @GetMapping("/cart.html")
-    public String cartListPage( ) {
+    public String cartListPage() {
 
         // 获取拦截器中的ThreadLocal中的数据
         UserInfoTo userInfoTo = CartInterceptor.threadLocal.get();
         System.out.println("userInfoTo = " + userInfoTo);
 
         return "cartList";
+    }
+
+    /**
+     * 添加商品到购物车
+     * @return
+     */
+    @GetMapping("/addToCart")
+    public String addToCart() {
+
+        return "success";
     }
 
 }
