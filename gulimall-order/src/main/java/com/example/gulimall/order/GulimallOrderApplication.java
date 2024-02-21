e26a3f8954b8a8e0aefb1803038ee5e335c7b882
taoao <taoao0101@163.com>
2024-02-22 00:53:24 +0800

    P256-商城业务-消息队列-SpringBoot整合RabbitMQ

--- gulimall-order/src/main/java/com/example/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/example/gulimall/order/GulimallOrderApplication.java
@@ -1,15 +1,15 @@
 package com.example.gulimall.order;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableRabbit  // 开启RabbitMQ
 public class GulimallOrderApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GulimallOrderApplication.class, args);
     }
-
 }
--- gulimall-order/src/test/java/com/example/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/example/gulimall/order/GulimallOrderApplicationTests.java
@@ -3,16 +3,27 @@ package com.example.gulimall.order;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class GulimallOrderApplicationTests {
+public class GulimallOrderApplicationTests {
 
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+    /**
+     * 1、如何创建Exchange、Queue、Binding
+     *      1）、使用AmqpAdmin进行创建
+     * 2、如何收发消息
+     */
     @Test
-    void contextLoads() {
+    void createExchange() {
+
     }
 
 }
