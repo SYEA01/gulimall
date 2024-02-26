5f073e764cbf2005b20c65d524900809724315f8
taoao <taoao0101@163.com>
2024-02-27 00:41:18 +0800

    P260-商城业务-消息队列-可靠投递-发送端确认（确认消息成功发送到exchange、确认消息成功发送到queue）

--- gulimall-order/src/main/java/com/example/gulimall/order/config/MyRabbitConfig.java
+++ gulimall-order/src/main/java/com/example/gulimall/order/config/MyRabbitConfig.java
@@ -1,9 +1,15 @@
 package com.example.gulimall.order.config;
 
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author taoao
@@ -11,9 +17,62 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyRabbitConfig {
 
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    /**
+     * 定制RabbitTemplate  【 设置确认回调 】
+     * 1、服务器收到消息就回调
+     */
+    @PostConstruct  // 在MyRabbitConfig这个配置类的构造器创建完（MyRabbitConfig对象创建完）之后，调用这个方法
+    public void initRabbitTemplate() {
+        // 设置确认回调
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            /**
+             * 当交换机收到消息，就会自动回调这个方法
+             * 1、只要消息抵达Broker，ack就是true
+             * @param correlationData correlation data for the callback. 当前消息的唯一关联数据【 唯一id 】
+             * @param ack true for ack, false for nack 消息是否成功收到
+             * @param cause An optional cause, for nack, when available, otherwise null. 失败的原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm...correlationData[" + correlationData + "]===>ack[" + ack + "]===>cause[" + cause + "]");
+            }
+
+        });
+
+        /**
+         * 设置消息抵达队列的确认回调
+         * 1、只要消息抵达queue
+         */
+        rabbitTemplate.setReturnCallback(new RabbitTemplate.ReturnCallback() {
+            /**
+             * 失败回调 【 消息抵达queue不会触发 】
+             * 只要消息没有抵达queue，就触发这个失败回调
+             * @param message the returned message. 哪个消息投递到queue失败，消息的详细信息
+             * @param replyCode the reply code.  回复的状态码
+             * @param replyText the reply text. 回复的文本内容
+             * @param exchange the exchange. 当时这个消息发给哪个交换机
+             * @param routingKey the routing key. 当时这个消息用的哪个路由键
+             */
+            @Override
+            public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+                System.out.println("Fail Message[" + message + "]==>replyCode[" + replyCode + "]===>replyText[" + replyText + "]===>exchange[" + exchange + "]==>routingKey[" + routingKey + "]");
+            }
+        });
+    }
+
+
+    /**
+     * 使用JSON序列化机制，进行消息转换
+     *
+     * @return
+     */
     @Bean
-    public MessageConverter messageConverter(){
+    public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
+
 
 }
