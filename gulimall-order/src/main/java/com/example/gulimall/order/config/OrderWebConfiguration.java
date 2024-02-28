d83bede557951aeca023d862cfaee15554bbffb5
taoao <taoao0101@163.com>
2024-02-28 23:34:36 +0800

    P265-商城业务-订单服务-订单未登录拦截

---
+++ gulimall-order/src/main/java/com/example/gulimall/order/config/OrderWebConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.gulimall.order.config;
+
+import com.example.gulimall.order.interceptor.LoginUserInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author taoao
+ */
+@Configuration
+public class OrderWebConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    LoginUserInterceptor loginUserInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginUserInterceptor).addPathPatterns("/**");
+    }
+}
---
+++ gulimall-order/src/main/java/com/example/gulimall/order/interceptor/LoginUserInterceptor.java
@@ -0,0 +1,35 @@
+package com.example.gulimall.order.interceptor;
+
+import com.example.common.constant.AuthServerConstant;
+import com.example.common.vo.MemberRespVo;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 拦截去
+ *
+ * @author taoao
+ */
+@Component
+public class LoginUserInterceptor implements HandlerInterceptor {
+
+    public static ThreadLocal<MemberRespVo> loginUser = new ThreadLocal<>();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MemberRespVo attribute = (MemberRespVo) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
+        if (attribute != null) {
+            // 用户登录了
+            loginUser.set(attribute);
+            return true;
+        } else {
+            // 没登录，就去登录
+            request.getSession().setAttribute("msg","请先进行登录。");
+            response.sendRedirect("http://auth.gulimall.com/login.html");
+            return false;
+        }
+    }
+}
