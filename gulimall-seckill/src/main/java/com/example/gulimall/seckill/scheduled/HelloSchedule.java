ce6db23e2f67051720f224ccb9098169d23d0525
taoao <taoao0101@163.com>
2024-03-20 23:15:23 +0800

    P313-2-商城业务-秒杀服务-SpringBoot整合异步任务

--- gulimall-seckill/src/main/java/com/example/gulimall/seckill/scheduled/HelloSchedule.java
+++ gulimall-seckill/src/main/java/com/example/gulimall/seckill/scheduled/HelloSchedule.java
@@ -1,21 +1,28 @@
 package com.example.gulimall.seckill.scheduled;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
  * 定时任务：
- *      1、@EnableScheduling  // 开启定时任务功能
- *      2、@Scheduled(cron = "* * * ? * 3")  // 开启一个定时任务
+ *      1、类上标注 @EnableScheduling  // 开启定时任务功能
+ *      2、方法上标注 @Scheduled(cron = "* * * ? * 3")  // 开启一个定时任务
+ *      3、自动配置类：TaskSchedulingAutoConfiguration
  *
- *
+ * 异步任务：
+ *      1、类上标注 @EnableAsync  // 开启异步任务功能
+ *      2、方法上标注 @Async  // 异步执行这个方法
+ *      3、自动配置类：TaskExecutionAutoConfiguration。 它的属性绑定在：TaskExecutionProperties 类中
  * @author taoao
  */
 @EnableScheduling  // 开启定时任务功能
 @Component
 @Slf4j
+@EnableAsync  // 开启异步任务功能
 public class HelloSchedule {
 
     /**
@@ -27,9 +34,11 @@ public class HelloSchedule {
      *          方式2）、支持定时任务线程池  通过spring.task.scheduling.pool.size来配置线程池的数量
      *          方式3）、让定时任务异步执行，
      *              异步任务：
+     *      使用定时任务 + 异步任务来解决定时任务不阻塞的功能
      */
     // 秒 分 时 日 月 周
     @Scheduled(cron = "* * * ? * 3")  // 开启一个定时任务
+    @Async  // 异步执行这个方法
     public void hello() throws InterruptedException {
         log.info("hello...");
         Thread.sleep(3000);
