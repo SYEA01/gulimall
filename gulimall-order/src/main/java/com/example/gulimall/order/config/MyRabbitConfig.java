23403aa8dbe6ef407399b828aea5cdc72520298a
taoao <taoao0101@163.com>
2024-03-25 00:07:18 +0800

    P330-sentinel-全服务引入

--- gulimall-order/src/main/java/com/example/gulimall/order/config/MyRabbitConfig.java
+++ gulimall-order/src/main/java/com/example/gulimall/order/config/MyRabbitConfig.java
@@ -1,6 +1,7 @@
 package com.example.gulimall.order.config;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -8,6 +9,7 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.annotation.PostConstruct;
 
@@ -17,14 +19,25 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class MyRabbitConfig {
 
-    @Autowired
+    //    @Autowired
     RabbitTemplate rabbitTemplate;
+
+    // TODO
+    @Primary
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setMessageConverter(messageConverter());
+        initRabbitTemplate();
+        return rabbitTemplate;
+    }
 
     /**
      * 定制RabbitTemplate  【 设置确认回调 】
      * 1、服务器收到消息就回调
      */
-    @PostConstruct  // 在MyRabbitConfig这个配置类的构造器创建完（MyRabbitConfig对象创建完）之后，调用这个方法
+//    @PostConstruct  // 在MyRabbitConfig这个配置类的构造器创建完（MyRabbitConfig对象创建完）之后，调用这个方法
     public void initRabbitTemplate() {
         // 设置确认回调
         rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
@@ -39,7 +52,7 @@ public class MyRabbitConfig {
             public void confirm(CorrelationData correlationData, boolean ack, String cause) {
                 // 服务器收到了
                 // 修改消息的状态
-                System.out.println("confirm...correlationData[" + correlationData + "]===>ack[" + ack + "]===>cause[" + cause  + "]");
+                System.out.println("confirm...correlationData[" + correlationData + "]===>ack[" + ack + "]===>cause[" + cause + "]");
             }
 
         });
