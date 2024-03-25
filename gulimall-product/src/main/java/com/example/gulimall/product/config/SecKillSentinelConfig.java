83a2c7c5f221b61018b6ec6824ecc826180070e6
taoao <taoao0101@163.com>
2024-03-26 00:18:30 +0800

    P332-3-sentinel-给每一个服务都加上sentinel的依赖和限流时的配置【默认响应】

---
+++ gulimall-product/src/main/java/com/example/gulimall/product/config/SecKillSentinelConfig.java
@@ -0,0 +1,42 @@
+package com.example.gulimall.product.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
+import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSON;
+import com.example.common.exception.BizCodeEnume;
+import com.example.common.utils.R;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 自定义sentinel的流控响应
+ */
+@Configuration
+public class SecKillSentinelConfig {
+
+    /**
+     * 在构造方法中添加一个web的回调管理器
+     */
+    public SecKillSentinelConfig() {
+        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
+            /**
+             * 在这个方法中，自定义请求过多的返回数据
+             * @param httpServletRequest
+             * @param httpServletResponse
+             * @param e
+             * @throws IOException
+             */
+            @Override
+            public void blocked(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, BlockException e) throws IOException {
+                R error = R.error(BizCodeEnume.TO_MANY_REQUEST.getCode(), BizCodeEnume.TO_MANY_REQUEST.getMessage());
+                httpServletResponse.setCharacterEncoding("UTF-8");
+                httpServletResponse.setContentType("application/json");
+                httpServletResponse.getWriter().write(JSON.toJSONString(error));
+            }
+        });
+    }
+}
